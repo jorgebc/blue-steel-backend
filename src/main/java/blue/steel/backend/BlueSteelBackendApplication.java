8f8f5b45b556413c9ac691b45729b4ab128304d9
Jorge Santiago Buffa Casalis <jorge.buffa.casalis@gmail.com>
2022-01-13 20:23:29 +0100

    created project through spring.io

---
+++ src/main/java/blue/steel/backend/BlueSteelBackendApplication.java
@@ -0,0 +1,16 @@
+package blue.steel.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main spring boot application class.
+ */
+@SpringBootApplication
+public class BlueSteelBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlueSteelBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/blue/steel/backend/BlueSteelBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package blue.steel.backend;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueSteelBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertTrue(true, "Context loads");
+  }
+
+}
