f230ae626c27a4d31e024f8368b7306ea65a77f5
Jorge Santiago Buffa Casalis <jorge.buffa.casalis@gmail.com>
2022-02-13 14:20:43 +0100

    auth0 authorization

--- src/main/java/blue/steel/backend/core/config/SecurityConfig.java
+++ src/main/java/blue/steel/backend/core/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +21,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /** Spring security configuration. */
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Value("${auth0.audience}")
